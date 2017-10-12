1822b1a8c5a3d849144c205c5bc3714a4f262ae5
rachnerd <Takkie1991>
2017-10-12 16:28:19 +0200

    Moved bonus assignment

--- src/test/java/nl/quintor/qrxjava/HotAssignments.java
+++ src/test/java/nl/quintor/qrxjava/HotAssignments.java
@@ -2,6 +2,7 @@ package nl.quintor.qrxjava;
 
 import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observables.GroupedObservable;
 import io.reactivex.schedulers.TestScheduler;
 import io.reactivex.subjects.BehaviorSubject;
 import io.reactivex.subjects.PublishSubject;
@@ -10,6 +11,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -192,5 +194,37 @@ public class HotAssignments {
         //---------end--------------
 
         Assert.assertEquals(number.get(), 1);
+    }
+
+    /**
+     * Bonus
+     */
+
+    @Test
+    public void assignment9() {
+        Observable<Integer> allNumbers$ = Observable.just(1, 2, 3, 4, 5);
+        List<Integer> even = new ArrayList<>();
+        List<Integer> odd = new ArrayList<>();
+
+        //---------start----------------
+
+        Observable<GroupedObservable<Boolean, Integer>> groupedEvenNumbers = allNumbers$
+                .groupBy(n -> n % 2 == 0);
+
+        groupedEvenNumbers
+                .filter(GroupedObservable::getKey)
+                .switchMap(g -> g)
+                .subscribe(even::add);
+
+        groupedEvenNumbers
+                .filter(g -> !g.getKey())
+                .switchMap(g -> g)
+                .subscribe(odd::add);
+
+        //----------end-----------------
+
+        Assert.assertEquals(even, Arrays.asList(2, 4));
+        Assert.assertEquals(odd, Arrays.asList(1, 3, 5));
+
     }
 }
